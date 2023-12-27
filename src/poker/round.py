393518651f886c3d2e9e9187afe899efcb972e9c
Sergei Volkov <serg-v@toloka.ai>
2023-12-27 13:06:32 +0100

    adding deal

--- src/poker/round.py
+++ src/poker/round.py
@@ -1,19 +1,71 @@
+import random
+
+
+seed = None
+
+
 class Round:
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args,  **kwargs):
         self.hands = [Hand(arg) for arg in args]
 
     def stats(self):
         return "\n".join(
-            f"{i + 1}: {hand.cards}: {hand.stats()}"
+            f"{i + 1}: {hand.definitions}: {hand.stats()}"
+            for i, hand in enumerate(self.hands)
+        )
+
+    def deal(self):
+        deck = Deck()
+        for hand in self.hands:
+            hand.deal(deck)
+        return "\n".join(
+            f"{i + 1}: {hand.cards[0]} {hand.cards[1]}"
             for i, hand in enumerate(self.hands)
         )
 
 
+class Card:
+
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+    def __str__(self):
+        return f"{self.rank}{self.suit}"
+
+
+def new_deck():
+    for suit in ["♠", "♣", "♥", "♦"]:
+        for rank in ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]:
+            yield Card(rank, suit)
+
+
+class Deck:
+
+    def __init__(self):
+        if seed is not None:
+            random.seed(seed)
+        self.cards = list(new_deck())
+        self.shuffle()
+
+    def deal(self, definitions):
+        return [self.cards.pop() for _ in range(len(definitions))]
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+
 class Hand:
 
-    def __init__(self, cards):
-        self.cards = cards
+    def __init__(self, definitions):
+        self.cards = None
+        self.definitions = definitions
 
     def stats(self):
         return 0.9
+
+    def deal(self, deck: Deck):
+        self.cards = deck.deal(self.definitions)
+
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,20 @@
+import poker.round
 from poker import Round
 
 
-def test_sample():
+poker.round.seed = 42
+
+
+# FIXME: This stats are incorrect
+def test_stats():
     assert Round("AKs", "??").stats() == """
 1: AKs: 0.9
 2: ??: 0.9
+    """.strip()
+
+
+def test_deal():
+    assert Round("AKs", "??").deal() == """
+1: K♦ 7♠
+2: 6♦ T♣
     """.strip()
